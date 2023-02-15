5edacb17332c0e5dfdede9258eacb4aa8d053255
tashisrael <tashisrael04@gmail.com>
2023-02-15 14:33:22 +0300

    add recipe table

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -1,15 +1,12 @@
 class FoodsController < ApplicationController
-  # GET /foods or /foods.json
   def index
     @foods = current_user.foods
   end
 
-  # GET /foods/1 or /foods/1.json
   def show
     @food = Food.find(params[:id])
   end
 
-  # GET /foods/new
   def new
     @food = Food.new
   end
@@ -18,7 +15,6 @@ class FoodsController < ApplicationController
     @food = Food.find(params[:id])
   end
 
-  # POST /foods or /foods.json
   def create
     @food = current_user.foods.new(food_params)
 
@@ -26,12 +22,11 @@ class FoodsController < ApplicationController
       if @food.save
         format.html { redirect_to foods_path, notice: 'Food was successfully created.' }
       else
-        flash[:alert] = 'Oops, Loser try again!'
+        flash[:alert] = 'Food od registration unsuccessful'
       end
     end
   end
 
-  # DELETE /foods/1 or /foods/1.json
   def destroy
     food = Food.find(params[:id])
     respond_to do |format|
@@ -45,7 +40,6 @@ class FoodsController < ApplicationController
 
   private
 
-  # Only allow a list of trusted parameters through.
   def food_params
     params.require(:food).permit(:name, :measurement_unit, :price, :quantity)
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_02_15_075959) do
+ActiveRecord::Schema[7.0].define(version: 2023_02_15_184533) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -22,6 +22,25 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_15_075959) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_foods_on_user_id"
+  end
+
+  create_table "recipe_foods", force: :cascade do |t|
+    t.bigint "recipe_id", null: false
+    t.bigint "food_id", null: false
+    t.integer "quantity"
+    t.index ["recipe_id", "food_id"], name: "index_recipe_foods_on_recipe_id_and_food_id"
+  end
+
+  create_table "recipes", force: :cascade do |t|
+    t.string "name"
+    t.string "preparation_time"
+    t.string "cooking_time"
+    t.text "description"
+    t.boolean "public", default: false
+    t.bigint "users_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["users_id"], name: "index_recipes_on_users_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -43,4 +62,5 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_15_075959) do
   end
 
   add_foreign_key "foods", "users"
+  add_foreign_key "recipes", "users", column: "users_id"
 end

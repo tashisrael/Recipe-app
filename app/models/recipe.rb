6f4bf55b2638906351110b50da7602bfec4574a9
MohamedCK <abdulhakimm280@gmail.com>
2023-02-16 11:05:57 +0200

    - Fix linter errors

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,7 +1,6 @@
 class Recipe < ApplicationRecord
-    belongs_to :user
-  
-    validates :name, presence: true, length: { minimum: 1, maximum: 100 }
-    validates :description, presence: true, length: { minimum: 1, maximum: 1000 }
+  belongs_to :user
+
+  validates :name, presence: true, length: { minimum: 1, maximum: 100 }
+  validates :description, presence: true, length: { minimum: 1, maximum: 1000 }
 end
-  

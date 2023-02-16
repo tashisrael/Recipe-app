3937fa254430bc654cedf8a3bc93fde72b5edb94
MohamedCK <abdulhakimm280@gmail.com>
2023-02-17 00:20:49 +0200

    - Update the models

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,11 @@
 class User < ApplicationRecord
-  has_many :recipes, foreign_key: 'user_id'
+  has_many :recipes, foreign_key: 'user_id', dependent: :destroy
+  has_many :foods, foreign_key: 'user_id', dependent: :destroy
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :confirmable
 
-  has_many :foods, dependent: :destroy
   validates :name, presence: true, length: { minimum: 1, maximum: 100 }
 end

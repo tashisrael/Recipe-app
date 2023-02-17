21171a9c43bd2885181f8a8dac11e4244dcf86d7
MohamedCK <abdulhakimm280@gmail.com>
2023-02-17 17:54:39 +0200

    - Write rspec tests for the models

---
+++ spec/models/recipe_spec.rb
@@ -0,0 +1,39 @@
+require 'rails_helper'
+
+RSpec.describe Recipe, type: :model do
+  before :each do
+    @user = User.new(name: 'Test', email: 'test@test.com',  password: 'password', password_confirmation: 'password')
+    @user.skip_confirmation!
+    @user.save
+  end
+
+  it 'is valid' do
+    recipe = Recipe.new(name: 'Miso Paste', preparation_time: '120', cooking_time: '210',
+                        description: 'Test', public: false, user_id: @user.id)
+    expect(recipe).to be_valid
+  end
+
+  it 'should not be valid without a name' do
+    recipe = Recipe.new(name: nil, preparation_time: '145', cooking_time: '154', description: 'Test',
+                        public: false, user_id: @user.id)
+    expect(recipe).to_not be_valid
+  end
+
+  it 'should not be valid without cooking time' do
+    recipe = Recipe.new(name: 'Miso Paste', preparation_time: '6 months', cooking_time: nil, description: 'Test',
+                        public: false, user_id: @user.id)
+    expect(recipe).to_not be_valid
+  end
+
+  it 'should not be valid without preparation time' do
+    recipe = Recipe.new(name: 'Miso Paste', preparation_time: nil, cooking_time: '2 hours', description: 'Test',
+                        public: false, user_id: @user.id)
+    expect(recipe).to_not be_valid
+  end
+
+  it 'should not be valid without a description' do
+    recipe = Recipe.new(name: 'Miso Paste', preparation_time: '6 months', cooking_time: '2 hours',
+                        description: nil, public: false, user_id: @user.id)
+    expect(recipe).to_not be_valid
+  end
+end
